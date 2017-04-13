f779576eebc2a1b878af19ae8ecbe23159ef26aa
kibernetik542 <qwerty5798858@box.az>
2017-04-13 21:46:16 +0400

    Add project files.

---
+++ SpecsMVC5/Controllers/ProductController.cs
@@ -0,0 +1,45 @@
+﻿using SpecsMVC5.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace SpecsMVC5.Controllers
+{
+    public class ProductController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            var products = db.Products.ToList();
+            return View(products);
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "CategoryName");
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            db.Products.Add(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpPost]
+        public void Delete(int id)
+        {
+            Product product = db.Products.FirstOrDefault(x => x.ProductId == id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+        }
+
+    }
+}
---
+++ SpecsMVC5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SpecsMVC5.MvcApplication" Language="C#" %>
